390d37fd2d02f5f0876881ea0b574ac945cd3fa3
christ khodabakhshi <christ.khodabakhshi@gmail.com>
2016-05-03 00:15:33 -0500

    add blurb to Post to have a short description for showing in posts and meta description

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
+		@posts = Post.all
 	end
 
 	def new
@@ -40,7 +41,7 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :content, :slug)
+		params.require(:post).permit(:title, :content, :slug, :blurb)
 	end
 
 	def find_post
