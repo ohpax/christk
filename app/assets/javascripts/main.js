5a23146393f73912fcfaba14288f1f2116fa5c6e
christ khodabakhshi <christ.khodabakhshi@gmail.com>
2016-05-05 19:53:56 -0500

    bring every thing back about fonts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -39,15 +39,3 @@ $(document).on('ready page:load', function() {
 });
 
 
-<script type="text/javascript">
-  WebFontConfig = {
-    google: { families: [ 'Comfortaa::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); </script>
